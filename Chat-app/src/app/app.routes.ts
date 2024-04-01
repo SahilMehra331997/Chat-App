41fad204f1eabaeed030d440a6fa4b2d669f4509
sahil-mehra <mehrasahil331997@gmail.com>
2024-04-01 17:23:53 +0530

    Login component

--- Chat-app/src/app/app.routes.ts
+++ Chat-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './MyComponents/landing/landing.component';
+import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { HomeComponent } from './MyComponents/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:"",pathMatch:"full",component:LandingComponent},
+                               {path:"sign-up",component:SignUpComponent},
+                               {path:"login",component:LoginComponent},
+                               {path:"home",component:HomeComponent}
+                            ];
