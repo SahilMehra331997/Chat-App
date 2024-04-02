349cc09c6bd8e81ecbedff3bed9be2e234349daf
sahil-mehra <mehrasahil331997@gmail.com>
2024-04-02 14:31:47 +0530

    Firebase added

--- chatapp/src/app/app.config.ts
+++ chatapp/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"chat-app-d0f72","appId":"1:996808410885:web:447ba795d9f6d18bde196c","storageBucket":"chat-app-d0f72.appspot.com","apiKey":"AIzaSyCl0pY22VoyEADOAPF95Ph1UZWXfPyE9T0","authDomain":"chat-app-d0f72.firebaseapp.com","messagingSenderId":"996808410885"}))), importProvidersFrom(provideAuth(() => getAuth()))]
 };
